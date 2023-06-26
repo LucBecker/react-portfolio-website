9af90e035e65d4f56506215386fd18dd7f6bcdbd
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-26 10:28:17 -0300

    Floating Nav Component JSX

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com/LucBecker' target="_blank"><FaGithub/></a>
-        <a href='https://www.instagram.com/lucass_becker/' target="_blank"><BsInstagram/></a>
+        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href='https://github.com/LucBecker' target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href='https://www.instagram.com/lucass_becker/' target="_blank" rel="noreferrer"><BsInstagram/></a>
 
     </div>
   )
