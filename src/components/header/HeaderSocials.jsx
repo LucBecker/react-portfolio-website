418c4d0d46046666fbb73e853eb58f5869a58d17
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-26 00:23:31 -0300

    Installing React Icons and Using them

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"></a>
-        <a href='https://github.com/LucBecker' target="_blank"></a>
-        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"></a>
+        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"><BsLinkedin/></a>
+        <a href='https://github.com/LucBecker' target="_blank"><FaGithub/></a>
+        <a href='https://www.instagram.com/lucass_becker/' target="_blank"><BsInstagram/></a>
 
     </div>
   )
