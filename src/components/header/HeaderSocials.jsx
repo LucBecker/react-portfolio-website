d41ffbbaf30c4ac2760a724603dc21daedae685e
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-26 00:08:24 -0300

    Header Component JSX

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"></a>
+        <a href='https://github.com/LucBecker' target="_blank"></a>
+        <a href='https://www.linkedin.com/in/lucas-becker-712237169/' target="_blank"></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
