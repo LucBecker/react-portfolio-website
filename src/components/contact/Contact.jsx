8439d37f88ba660b8974d064516d0b451356f26a
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-27 23:46:20 -0300

    Contact Component JSX (Multiple contact options: social media

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {AiOutlineWhatsApp} from 'react-icons/ai'
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contato</section>
+    <section id='contact'>
+      <h5>Vamos conversar!</h5>
+      <h2>Contato</h2>
+
+      <div className='container .contact__container'>
+          <div className='contact__options'>
+            <article className='contact__option'>
+              <HiOutlineMail/>
+              <h4>Email</h4>
+              <h5>lks.becker@gmail.com</h5>
+              <a href='mailto:lks.becker@gmail.com' target="_blank">Enviar uma mensagem</a>
+            </article>
+
+            <article className='contact__option'>
+              <RiMessengerLine/>
+              <h4>Messeger</h4>
+              <h5>Lucas Becker</h5>
+              <a href='https://m.me/lucas.becker' target="_blank">Enviar uma mensagem</a>
+            </article>
+
+            <article className='contact__option'>
+              <AiOutlineWhatsApp/>
+              <h4>WhatsApp</h4>
+              <h5>+5555981072226</h5>
+              <a href='https://api.whatsapp.com/send?phone=5555981072226' target="_blank">Enviar uma mensagem</a>
+            </article>
+          </div>
+          {/* END OF CONTACT OPTIONS*/}
+          <form action=''>
+            <input type='text' name='name' placeholder='Nome' required></input>
+            <input type='email' name='email' placeholder='E-mail' required></input>
+            <textarea name='message' rows='7' placeholder='Mensagem' required></textarea>
+            <button type='submit' className='btn btn-primary'>Enviar Mensagem</button>
+          </form>
+      </div>
+    </section>
   )
 }
 
