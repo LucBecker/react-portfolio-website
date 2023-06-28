676524435cab6fd977595f6848b9ea259a4a18b0
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-28 20:50:07 -0300

    Update photos and recommendations

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -19,27 +19,27 @@ const data = [
   
   {
   avatar: AVTR1,
-  name: 'Heleno da Silva Souza',
+  name: 'Bruno Ziantonio Afanasiev',
   occupation: 'Fullstack Software Engineer | Spring Boot | API REST | Hibernate | Nexus | Java | SQL',
   review: 'Extremamente apaixonado pelo que faz, muito atento aos detalhes e competente. Além das suas habilidades e experiências curriculares, ele constrói confiança nas suas relações profissionais e conduz suas atribuições com total maestria e sempre em busca de inovações.. Leva a informação de forma clara e eficiente a todos os públicos e níveis da organização. É sempre um prazer para mim dividir um convívio profissional com a Lucas.'
   },  
   {
     avatar: AVTR2,
-    name: 'Luan David Carrilho',
+    name: 'Lucas Batista',
     occupation: 'Software Developer | Dart | Flutter',
     review: 'O Lucas possui fortes competências técnicas, possui qualidades enquanto pessoa, fundamentais em qualquer organização. Destaco a sua força de vontade, empatia, curiosidade e excelente capacidade de comunicação. Estudamos juntos e trabalhamos juntos em alguns projetos de softwares durante 5 anos e considero que ele tem todas as condições para evoluir rapidamente na sua carreira, recomendo-o vivamente para todas as funções que possam exigir skills técnicos na área de IT, perfil de liderança, resiliência e cultura de organização.'
   },
   {
     avatar: AVTR3,
-    name: 'Kenny Wilker',
-    occupation: 'Developer | Java | Python | HTML | CSS | Javascript | Git/GitHub | Data Science & Analytics',
+    name: 'Mauro Pereira dos Santos',
+    occupation: 'Developer | Java | Python | HTML | CSS | Javascript | Git/GitHub',
     review: 'Extremamente apaixonado pelo que faz, muito atento aos detalhes e competente. Além das suas habilidades e experiências curriculares, ele constrói confiança nas suas relações profissionais e conduz suas atribuições com total maestria e sempre em busca de inovações.. Leva a informação de forma clara e eficiente a todos os públicos e níveis da organização. É sempre um prazer para mim dividir um convívio profissional com a Lucas.'
   },
   {
     avatar: AVTR4,
-    name: 'Danrley Santos',
+    name: 'Alan da Silva Ferreira',
     occupation: 'Software Engineer | Front End | React, React Native, Next.js, Typescript, and Node.js',
-    review: 'Lucas possui fortes habilidades em programação, lógica de programação e desenvolvimento de aplicações utilizando melhores práticas do mercado. Possui experiência em sala de aula e aptidão em resolver problemas.'
+    review: 'Lucas possui fortes habilidades em programação, lógica de programação e desenvolvimento de aplicações utilizando melhores práticas do mercado. Possui aptidão em resolver problemas.'
   }
 
 ]
@@ -65,6 +65,7 @@ const Testimonials = () => {
             <img src={avatar} alt={name}/>
             </div>
             <h5 className='client__name'>{name}</h5>
+            <h6 className='client__name'>{occupation}</h6>
             <small className='client__review'>{review}</small>
             </SwiperSlide>)
             })
